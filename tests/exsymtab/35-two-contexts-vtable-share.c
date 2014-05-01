459bf69328186e51515ed6da23a03fee7cff78c4
David Mertens <dcmertens.perl@gmail.com>
2014-05-01 14:37:25 -0400

    Get rid of pointer casting in test 30

--- tests/exsymtab/35-two-contexts-vtable-share.c
+++ tests/exsymtab/35-two-contexts-vtable-share.c
@@ -13,20 +13,26 @@ char first_code[] =
 "typedef unsigned long __uint64_t, uint64_t;\n"
 "#include <stdlib.h>\n"
 "struct point {\n"
+"    int (*squared_distance)(struct point * self);\n"
 "    int x;\n"
-"    int yval;\n"
+"    int y;\n"
 "};\n"
-"void * new_point(int x, int newy) {\n"
+"int point_squared_distance(struct point * self) {\n"
+"    return self->x * self->x + self->y * self->y;\n"
+"}\n"
+"void * new_point(int x, int y) {\n"
 "    struct point * pt = malloc(sizeof(struct point));\n"
+"    pt->squared_distance = point_squared_distance;\n"
 "    pt->x = x;\n"
-"    pt->yval = newy;\n"
+"    pt->y = y;\n"
 "    return pt;\n"
 "}\n"
 ;
 
 char second_code[] =
-"int sq_distance_to_pt(struct point * pt) {\n"
-"    return (pt->x) * (pt->x) + (pt->yval) * (pt->yval);\n"
+"int sq_distance_to_pt(void * pt_p) {\n"
+"    struct point * pt = pt_p;\n"
+"    return pt->squared_distance(pt);\n"
 "}\n"
 ;
 
@@ -39,7 +45,8 @@ int main(int argc, char **argv) {
 	setup_and_compile_s1(my_symtab, first_code);
 	SETUP_SECOND_CALLBACK_DATA;
 	
-	/* ---- Allocate a point and manually unpack it ---- */
+	/* ---- Allocate a point ---- */
+	
 	void* (*allocate_ptr)(int, int) = tcc_get_symbol(s1, "new_point");
 	if (allocate_ptr == NULL) return 1;
 	void * point_p = allocate_ptr(6, 9);
@@ -48,21 +55,31 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	else pass("Allocated point");
-	int * manual_unpack = point_p;
-	is_i(manual_unpack[0], 6, "manually unpacked x-value");
-	is_i(manual_unpack[1], 9, "manually unpacked y-value");
+	
+	/* ---- Check function address ---- */
+	
+	int (*squared_distance_func)(void*) = tcc_get_symbol(s1, "point_squared_distance");
+	void * func_p = ((void **)point_p)[0];
+	is_p(func_p, squared_distance_func, "Symbol table's function points to relocated location");
+	
+	/* ---- Make sure the function works correctly ---- */
+	is_i(squared_distance_func(point_p), 9*9+6*6, "Vtable function works correctly");
 	
 	/* ---- Compile the second compiler context ---- */
-	TCCState * s_second = tcc_new();
-	setup_and_relocate_second_state(s_second, second_code);
-	int (*sq_dist_ptr)(void*) = tcc_get_symbol(s_second, "sq_distance_to_pt");
-	if (sq_dist_ptr == NULL) return 1;
-	is_i(sq_dist_ptr(point_p), 9*9+6*6, "Second context able to unpack struct");
+	
+	if (func_p == squared_distance_func) {
+		TCCState * s_second = tcc_new();
+		setup_and_relocate_second_state(s_second, second_code);
+		int (*sq_dist_ptr)(void*) = tcc_get_symbol(s_second, "sq_distance_to_pt");
+		if (sq_dist_ptr == NULL) return 1;
+		is_i(sq_dist_ptr(point_p), 9*9+6*6, "Second context able to call function pointer from struct");
+		tcc_delete(s_second);
+	}
 	
 	/* ---- clean up the memory ---- */
+	
 	tcc_delete_extended_symbol_table(my_symtab);
 	tcc_delete(s1);
-	tcc_delete(s_second);
 	free(point_p);
 	pass("cleanup");
 	
