41f8bfde2d9b2f4166924faa2daaf2bdd27faa26
David Mertens <dcmertens.perl@gmail.com>
2014-06-17 14:28:58 -0400

    More tests

---
+++ tests/exsymtab/51-many-consuming-contexts-with-constants-early-delete.c
@@ -0,0 +1,111 @@
+/*
+ * Define a function once, use it repeatedly, deleting tcc states as
+ * soon as possible.
+ */
+
+/* uncomment to enable diagnostic output */
+//	#define DIAG(...) diag(__VA_ARGS__)
+
+#include "test_setup.h"
+
+int fib (int n);
+char buffer[200];
+int fib (int n) {
+    if (n <= 2)
+        return 1;
+    else
+        return fib(n-1) + fib(n-2);
+}
+
+char def_code[] =
+"int fib(int n);\n"
+"int fib_of_5() { return fib(5); }\n"
+;
+
+char first_code[] =
+"int twice_fib_of_5()\n"
+"{\n"
+"    return 2 * fib_of_5();\n"
+"}\n"
+;
+
+char second_code[] =
+"int thrice_fib_of_5() {\n"
+"    return 3 * fib_of_5();\n"
+"}\n"
+;
+
+int main(int argc, char **argv) {
+
+	/* ---- Compile the first code string and setup the callback data ---- */
+	
+	TCCState *s1 = tcc_new();
+	SIMPLE_SETUP(s1);
+	TokenSym_p* my_symtab;
+	
+	/* Code taken from test_setup.h and modified to work with my own relocation
+	 * point. */
+	tcc_set_extended_symtab_callbacks(s1, &copy_symtab, NULL, NULL, &my_symtab);
+    if (tcc_compile_string(s1, def_code) == -1) return 1;
+    tcc_add_symbol(s1, "fib", fib);
+    int def_code_size = tcc_relocate(s1, 0);
+    void * code = malloc(def_code_size);
+    if (code == NULL) return 1;
+	if (tcc_relocate(s1, code) == -1) return 1;
+	pass("First code string compiled and relocated fine");
+	
+	/* ---- Get the fib_of_5 function and evaluate it ---- */
+	
+	int (*fib5)() = tcc_get_symbol(s1, "fib_of_5");
+	if (fib5 == NULL) return -1;
+	pass("Found fib_of_5");
+	is_i(fib5(), 5, "Calling fib_of_5 from defining compiler context works");
+	
+	/* ---- Delete the original tcc state ---- */
+	
+	tcc_delete(s1);
+	
+	/* ---- Compile the second string ---- */
+	
+	SETUP_SECOND_CALLBACK_DATA;
+	TCCState *s_first = tcc_new();
+	setup_and_compile_second_state(s_first, first_code);
+	tcc_add_symbol(s_first, "fib_of_5", fib5);
+	relocate_second_state(s_first);
+	
+	/* ---- Check the functionality ---- */
+	
+	/* Retrieve twice_fib_of_5 */
+	int (*fib5x2)() = tcc_get_symbol(s_first, "twice_fib_of_5");
+	if (fib5x2 == NULL) return -1;
+	pass("Found twice_fib_of_5 function pointer");
+	is_i(fib5x2(), 10, "Dependent function call works");
+	
+	/* ---- Delete the first dependent state ---- */
+	
+	tcc_delete(s_first);
+	
+	/* ---- Compile the third string ---- */
+	
+	TCCState *s_second = tcc_new();
+	setup_and_compile_second_state(s_second, second_code);
+	tcc_add_symbol(s_second, "fib_of_5", fib5);
+	relocate_second_state(s_second);
+	
+	/* ---- Check the functionality ---- */
+	
+	int (*fib5x3)() = tcc_get_symbol(s_second, "thrice_fib_of_5");
+	if (fib5x3 == NULL) return -1;
+	pass("Found thrice_fib_of_5 function pointer");
+	is_i(fib5x3(), 15, "Second dependent fibonaci function call works");
+	
+	/* ---- Cleanup ---- */
+	tcc_delete_extended_symbol_table(my_symtab);
+	tcc_delete(s_second);
+	free(code);
+	pass("cleanup");
+	
+	done_testing();
+	
+	return 0;
+}
---
+++ tests/exsymtab/70-symtab-use-check.c
@@ -0,0 +1,125 @@
+/*
+ * This builds a symtab for a simple declaration and a full definition. Test
+ * number 10 verified that these agree, so the next step is to use the
+ * share the definition symtab and see if the dependent compilation makes any
+ * alterations.
+ */
+
+#include "test_setup.h"
+#include "tcc.h"
+
+char definition_code[] = "double foo(int bar, double *baz) { return 0; }\n";
+char declaration_code[] = "double foo(int bar, double *baz);\n";
+char consumer_code[] = "double mult_foo(double factor) { return factor * foo(0, 0); }";
+
+void compare_funcs(Sym * foo_decl, Sym * foo_def) {
+	is_i(foo_def->r, foo_decl->r, "foo->r agree");
+	is_i(foo_def->c, foo_decl->c, "foo->c agree");
+	is_i(foo_def->type.t, foo_decl->type.t, "foo->type.t agree");
+	
+	Sym * def_ret = foo_def->type.ref;
+	Sym * dec_ret = foo_decl->type.ref;
+	/* This is the only difference. The value 0x10000 indicates "This is just a
+	 * prototype," which means a later definition is allowed. I do not want a
+	 * later redefinition for functions that have been defined in an earlier
+	 * context, so these are allowed (and encouraged) to differ. */
+	is_i(dec_ret->r, 0x11000, "declaration ret->r is 0x11000");
+	is_i(def_ret->r, 0x1000,  "definition  ret->r is 0x01000");
+	is_i(def_ret->c, dec_ret->c, "ret->r agree");
+	is_i(def_ret->type.t, dec_ret->type.t, "ret->type.t agree");
+	
+	Sym * def_arg1 = def_ret->next;
+	Sym * dec_arg1 = dec_ret->next;
+	is_i(def_arg1->r, dec_arg1->r, "arg1->r agree");
+	is_i(def_arg1->c, dec_arg1->c, "arg1->c agree");
+	is_i(def_arg1->type.t, dec_arg1->type.t, "arg1->type.t agree");
+	
+	Sym * def_arg2 = def_arg1->next;
+	Sym * dec_arg2 = dec_arg1->next;
+	is_i(def_arg2->r, dec_arg2->r, "arg2->r agree");
+	is_i(def_arg2->c, dec_arg2->c, "arg2->c agree");
+	is_i(def_arg2->type.t, dec_arg2->type.t, "arg2->type.t agree");
+	
+	Sym * def_arg2pt = def_arg2->type.ref;
+	Sym * dec_arg2pt = dec_arg2->type.ref;
+	is_i(def_arg2pt->r, dec_arg2pt->r, "arg2pt->r agree");
+	is_i(def_arg2pt->c, dec_arg2pt->c, "arg2pt->c agree");
+	is_i(def_arg2pt->type.t, dec_arg2pt->type.t, "arg2pt->type.t agree");
+}
+
+int main(int argc, char **argv) {
+    TCCState *s_decl = tcc_new();
+    SIMPLE_SETUP(s_decl);
+    
+	/* Set the copy callback */
+	TokenSym_p* decl_symtab;
+	tcc_set_extended_symtab_callbacks(s_decl, &copy_symtab, NULL, NULL, &decl_symtab);
+	/* Compile */
+    if (tcc_compile_string(s_decl, declaration_code) == -1) return 1;
+    /* All done with that compiler, clean up */
+    tcc_free(s_decl);
+    
+    pass("Built declaration compiler state's symbol table");
+    
+    TCCState *s1 = tcc_new();
+	TokenSym_p* def_symtab;
+    setup_and_compile_s1(def_symtab, definition_code);
+    double (*foo_ptr)(double) = tcc_get_symbol(s1, "foo");
+    /* All done with that compiler, clean up */
+    tcc_free(s1);
+
+    /* get the symbol table layouts of the two */
+	int i;
+	Sym * foo_decl;
+	for (i = 0; i < tcc_tokensym_list_length(decl_symtab); i++) {
+		if (strcmp("foo", tcc_tokensym_name(decl_symtab[i])) == 0) {
+			if (decl_symtab[i]->sym_identifier == NULL) {
+				printf("foo TokenSym has no sym_identifier???\n");
+				return(1);
+			}
+			foo_decl = decl_symtab[i]->sym_identifier;
+		}
+	}
+    Sym * foo_def;
+    for (i = 0; i < tcc_tokensym_list_length(def_symtab); i++) {
+    	if (strcmp("foo", tcc_tokensym_name(def_symtab[i])) == 0) {
+    		if (def_symtab[i]->sym_identifier == NULL) {
+	    		printf("foo TokenSym has no sym_identifier???\n");
+	    		return(1);
+    		}
+    		foo_def = def_symtab[i]->sym_identifier;
+    	}
+    }
+    
+    diag("Before dependent compilation");
+    compare_funcs(foo_decl, foo_def);
+	
+	/* compile the dependent code block */
+	TCCState *s_consumer = tcc_new();
+	second_callback_data callback_data;
+	callback_data.first_symtab = def_symtab;
+	setup_and_compile_second_state(s_consumer, consumer_code);
+    
+    diag("After dependent compilation, before symbol addition");
+    compare_funcs(foo_decl, foo_def);
+	
+	tcc_add_symbol(s_consumer, "foo", foo_ptr);
+    
+    diag("After symbol addition, before relocation & free");
+    compare_funcs(foo_decl, foo_def);
+	
+	relocate_second_state(s_consumer);
+	tcc_free(s_consumer);
+	
+	/* ---- Compare them ---- */
+    diag("After dependent compilation");
+    compare_funcs(foo_decl, foo_def);
+	
+	/* Clean up */
+	tcc_delete_extended_symbol_table(def_symtab);
+	tcc_delete_extended_symbol_table(decl_symtab);
+	
+	done_testing();
+	
+	return 0;
+}
