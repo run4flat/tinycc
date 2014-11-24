a9f409f8aedc487af194ad8b4458ba927bed2662
David Mertens <dcmertens.perl@gmail.com>
2014-11-24 15:45:44 -0500

    Fix test 29 for new symtab API

--- tests/exsymtab/29-three-contexts-intertwined-preprocessor-macro.c
+++ tests/exsymtab/29-three-contexts-intertwined-preprocessor-macro.c
@@ -26,59 +26,67 @@ char third_code[] =
 "	return add_foo_and_var_to(4);\n"
 "}\n";
 
+typedef struct {
+	TCCState * oldest_context;
+	TCCState * middle_context;
+	TCCState * current_context;
+	TokenSym_p* oldest_symtab;
+	TokenSym_p* middle_symtab;
+} three_callback_data;
+
 void my_copy_symtab(TokenSym_p* copied_symtab, void * data) {
-	TokenSym_p** my_symtabs = (TokenSym_p**)data;
-	if (my_symtabs[0] == 0) my_symtabs[0] = copied_symtab;
-	else my_symtabs[1] = copied_symtab;
+	three_callback_data* my_data = (three_callback_data*)data;
+	if (my_data->oldest_symtab == 0) my_data->oldest_symtab = copied_symtab;
+	else my_data->middle_symtab = copied_symtab;
 }
 
-TokenSym_p my_lookup_by_name (char * name, int len, void * data, int is_identifier) {
+/* This takes a second_callback_data pointer and sets its fields based on the
+ * current three-callback-data content. We know from the value of middle_symtab
+ * whether the middle state has been compiled (nonzero) or not (null), and we
+ * use that as our state indicator. */
+void setup_mock_data (second_callback_data *mock, void * data) {
+	three_callback_data* my_data = (three_callback_data*)data;
+	if (my_data->middle_symtab == 0) {
+		/* This is the case when the middle context has not yet been established
+		 * and it is referring to the original context. */
+		mock->second_context = my_data->middle_context;
+		mock->first_context = my_data->oldest_context;
+		mock->first_symtab = my_data->oldest_symtab;
+	}
+	else {
+		/* This is the case when the middle context has been compiled and copied
+		 * and we are working on the third compilation unit. */
+		mock->second_context = my_data->current_context;
+		mock->first_context = my_data->middle_context;
+		mock->first_symtab = my_data->middle_symtab;
+	}
+}
+
+TokenSym_p my_lookup_by_name (char * name, int len, void * data,
+	TokenSym_p**containing_symtab
+) {
 	/* Simply wrap the testing infrastructure's call appropriately */
 	second_callback_data mock;
-	mock.second_context = 0;
-	TokenSym_p returned;
-	TokenSym_p** my_symtabs = (TokenSym_p**)data;
-	if (my_symtabs[0] != 0) {
-		mock.first_symtab = my_symtabs[0];
-		returned = lookup_by_name(name, len, &mock, is_identifier);
-		if (returned) return returned;
-	}
-	if (my_symtabs[1] != 0) {
-		mock.first_symtab = my_symtabs[1];
-		returned = lookup_by_name(name, len, &mock, is_identifier);
-		if (returned) return returned;
-	}
-	return NULL;
+	setup_mock_data(&mock, data);
+	return lookup_by_name(name, len, &mock, containing_symtab);
 }
 
-TokenSym_p my_lookup_by_number (int tok_id, void * data, int is_identifier) {
+void my_sym_used (char * name, int len, void * data) {
 	second_callback_data mock;
-	mock.second_context = 0;
-	TokenSym_p returned;
-	TokenSym_p** my_symtabs = (TokenSym_p**)data;
-	if (my_symtabs[0] != 0) {
-		mock.first_symtab = my_symtabs[0];
-		returned = lookup_by_number(tok_id, &mock, is_identifier);
-		if (returned) return returned;
-	}
-	if (my_symtabs[1] != 0) {
-		mock.first_symtab = my_symtabs[1];
-		returned = lookup_by_number(tok_id, &mock, is_identifier);
-		if (returned) return returned;
-	}
-	return NULL;
+	setup_mock_data(&mock, data);
+	sym_used(name, len, &mock);
 }
-	
 
 int main(int argc, char **argv) {
 	
-	TokenSym_p* my_symtabs[2] = { 0, 0 };
+	three_callback_data my_data = { 0, 0, 0, 0, 0 };
 	
 	/* ---- Compile the first code string and setup the callback data ---- */
 	
 	TCCState *s_first = tcc_new();
+	my_data.oldest_context = s_first;
 	SIMPLE_SETUP(s_first);
-	tcc_set_extended_symtab_callbacks(s_first, &my_copy_symtab, NULL, NULL, my_symtabs);
+	tcc_set_extended_symtab_callbacks(s_first, &my_copy_symtab, NULL, NULL, &my_data);
     if (tcc_compile_string(s_first, first_code) == -1) {
     	printf("Failed to compile\n");
     	return 1;
@@ -90,9 +98,10 @@ int main(int argc, char **argv) {
 	pass("First code string compiled and relocated fine");
 	
 	TCCState *s_second = tcc_new();
+	my_data.middle_context = s_second;
 	SIMPLE_SETUP(s_second);
 	tcc_set_extended_symtab_callbacks(s_second, &my_copy_symtab,
-		&my_lookup_by_name, &my_lookup_by_number, my_symtabs);
+		&my_lookup_by_name, &my_sym_used, &my_data);
     if (tcc_compile_string(s_second, second_code) == -1) {
     	printf("Failed to compile\n");
     	return 1;
@@ -104,9 +113,10 @@ int main(int argc, char **argv) {
 	pass("Second code string compiled and relocated fine");
 	
 	TCCState *s_third = tcc_new();
+	my_data.current_context = s_third;
 	SIMPLE_SETUP(s_third);
 	tcc_set_extended_symtab_callbacks(s_third, NULL,
-		&my_lookup_by_name, &my_lookup_by_number, my_symtabs);
+		&my_lookup_by_name, &my_sym_used, &my_data);
     if (tcc_compile_string(s_third, third_code) == -1) {
     	printf("Failed to compile\n");
     	return 1;
@@ -126,8 +136,8 @@ int main(int argc, char **argv) {
 	
 	/* ---- clean up the memory ---- */
 	
-	tcc_delete_extended_symbol_table(my_symtabs[0]);
-	tcc_delete_extended_symbol_table(my_symtabs[1]);
+	tcc_delete_extended_symbol_table(my_data.oldest_symtab);
+	tcc_delete_extended_symbol_table(my_data.middle_symtab);
 	tcc_delete(s_first);
 	tcc_delete(s_second);
 	tcc_delete(s_third);
