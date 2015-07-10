458c59efe1ef8dea9c5766c79502852fb73f8134
David Mertens <dcmertens.perl@gmail.com>
2015-07-10 15:49:30 -0400

    Made mytest.pl more useful

--- tests/exsymtab/mytest.pl
+++ tests/exsymtab/mytest.pl
@@ -5,10 +5,12 @@ use warnings;
 my $cmd = shift;
 die "Command must be 'prove', 'test', or 'dbg'\n"
 	unless $cmd =~ /prove|test|dbg/;
-my $test = shift;
-my $file = (glob("$test*.c"))[0];
-die "Unable to find test file for test number $test\n"
-	unless -f $file;
+
+# I can either specify the test number or the whole file
+# name.
+my $file = shift;
+$file = (glob("$file*.c"))[0] unless -f $file;
+die "Unable to find test file\n" unless -f $file;
 
 $file =~ s/\.c/.$cmd/;
 system (make => $file)
